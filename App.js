c5d2b48c392b795e4ac9e03f5044ad7377e81c92
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-23 13:38:56 +0500

    Creating Welcome Screen

--- App.js
+++ App.js
@@ -1,28 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Image, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          backgroundColor: "tomato",
-          height: 50,
-          width: 50,
-        }}
-      ></View>
-      <View
-        style={{
-          backgroundColor: "#42CBAC",
-          height: 50,
-          width: 50,
-        }}
-      ></View>
-      <Image
-        source={require("./assets/chair.jpg")}
-        style={{ height: "75%", width: "100%" }}
-      />
-
+      <Text>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -31,9 +13,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
-    flexWrap: "wrap",
-    backgroundColor: "#000",
-    justifyContent: "space-between",
   },
 });
