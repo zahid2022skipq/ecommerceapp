a240593df9813af4ffd8a21ab3d4633becc14e4f
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-23 13:53:24 +0500

    Register Button Added

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -8,6 +8,7 @@ function WelcomeScreen(props) {
       source={require("../assets/background.jpg")}
     >
       <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
     </ImageBackground>
   );
 }
@@ -21,6 +22,11 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 70,
     backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
   },
 });
 
