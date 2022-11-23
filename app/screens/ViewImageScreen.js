43bfab1576cf024e22e3b77f825c6944a0e12520
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-23 14:11:41 +0500

    Image added to our screen

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { Image } from "react-native";
+import { Image, StyleSheet } from "react-native";
 
 function ViewImageScreen(props) {
-  return <Image source={require("../assets/chair.jpg")} />;
+  return (
+    <Image
+      resizeMode="contain"
+      style={styles.image}
+      source={require("../assets/chair.jpg")}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    height: "100%",
+    width: "100%",
+  },
+});
 
 export default ViewImageScreen;
