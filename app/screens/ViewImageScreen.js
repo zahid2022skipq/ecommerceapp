62b628b9918e4d7b485ad0f57938530f63781dbd
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-23 14:14:06 +0500

    Adding parent view to hold image

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,17 +1,23 @@
 import React from "react";
-import { Image, StyleSheet } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 
 function ViewImageScreen(props) {
   return (
-    <Image
-      resizeMode="contain"
-      style={styles.image}
-      source={require("../assets/chair.jpg")}
-    />
+    <View style={styles.container}>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/chair.jpg")}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#000",
+    flex: 1,
+  },
   image: {
     height: "100%",
     width: "100%",
