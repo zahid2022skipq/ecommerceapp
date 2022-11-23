5464fe8f3feb188712eac7732168c13411d0969d
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-23 13:47:53 +0500

    Creating Buttons

--- App.js
+++ App.js
@@ -1,17 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+const styles = StyleSheet.create({});
