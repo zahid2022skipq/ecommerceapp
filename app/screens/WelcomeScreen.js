30dca02ef0f68195013b6b745c507af9576f2609
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-23 13:40:00 +0500

    Styling Welcome Screen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { ImageBackground } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
 
 function WelcomeScreen(props) {
   return (
     <ImageBackground
+      style={styles.background}
       source={require("../assets/background.jpg")}
     ></ImageBackground>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
+});
 
 export default WelcomeScreen;
