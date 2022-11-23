0ef01e533eaa106a4bae9d7dbc4fbba8478a2142
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-23 13:51:48 +0500

    Creating Buttons

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,18 +1,25 @@
 import React from "react";
-import { ImageBackground, StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
 
 function WelcomeScreen(props) {
   return (
     <ImageBackground
       style={styles.background}
       source={require("../assets/background.jpg")}
-    ></ImageBackground>
+    >
+      <View style={styles.loginButton}></View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
   },
 });
 
