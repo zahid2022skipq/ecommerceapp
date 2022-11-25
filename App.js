0d56bc13b3ddd74a9eec80e71fb4eae10040cea6
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-25 19:46:51 +0500

    Adding styles

--- App.js
+++ App.js
@@ -3,16 +3,18 @@ import { StyleSheet, Text, View } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import Card from "./app/components/Card";
+import ListingComponentScreen from "./app/screens/ListingComponentScreen";
+
+// <View style={{ backgroundColor: "#f8f4f4", padding: 20, paddingTop: 100 }}>
+// <Card
+//   title="Red Jacket for sell"
+//   subTitle="$100"
+//   image={require("./app/assets/jacket.jpg")}
+// />
+// </View>
+
 export default function App() {
-  return (
-    <View style={{ backgroundColor: "#f8f4f4", padding: 20, paddingTop: 100 }}>
-      <Card
-        title="Red Jacket for sell"
-        subTitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-      />
-    </View>
-  );
+  return <ListingComponentScreen />;
 }
 
 const styles = StyleSheet.create({});
