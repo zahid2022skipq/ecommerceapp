113b693ce78c1d492463e8a5e854a677af03de15
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-25 19:20:12 +0500

    Added Card Component

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,16 +1,23 @@
 import React from "react";
 
 import { View, StyleSheet, Text, Image } from "react-native";
+import { colors } from "../config/colors";
 
 function Card({ title, subTitle, image }) {
   return (
-    <View>
+    <View style={styles.card}>
       <Text>{title}</Text>
       <Text>{subTitle}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: colors.white,
+    borderRadius: 20,
+    marginBottom: 20,
+  },
+});
 
 export default Card;
