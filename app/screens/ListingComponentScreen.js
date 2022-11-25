51ef8db231de61e03b21aedd99978a2d7e9f6593
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-25 19:49:46 +0500

    Adding styles to our apptext

--- app/screens/ListingComponentScreen.js
+++ app/screens/ListingComponentScreen.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { Image, View, StyleSheet } from "react-native";
 
+import AppText from "../components/AppText";
+
 function ListingComponentScreen(props) {
   return (
     <View>
       <Image style={styles.image} source={require("../assets/jacket.jpg")} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subTitle}>{subTitle}</AppText>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  detailsContainer: {},
   image: {
     width: "100%",
     height: 200,
