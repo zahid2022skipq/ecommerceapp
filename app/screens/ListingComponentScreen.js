bf735caf78988ddd5f3acc0f3ad5d704762ccfdf
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-25 19:46:08 +0500

    Adding styles

--- app/screens/ListingComponentScreen.js
+++ app/screens/ListingComponentScreen.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { Image, View,StyleSheet } from "react-native";
+import { Image, View, StyleSheet } from "react-native";
 
 function ListingComponentScreen(props) {
-    return <View>
-    <Image style={StyleSheet.} source={require('../assets/jacket.jpg')} />
-    </View>;
+  return (
+    <View>
+      <Image style={styles.image} source={require("../assets/jacket.jpg")} />
+    </View>
+  );
 }
 
-
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 200,
+  },
+});
 
 export default ListingComponentScreen;
