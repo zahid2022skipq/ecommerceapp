3775ee5ec788880d0cca6ad73afea4fa39599c9f
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-26 14:25:47 +0500

    added static data object

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,6 +1,27 @@
 import React from "react";
 import { FlatList } from "react-native";
 
+const data = [
+  {
+    id: "1",
+    title: "T1",
+    description: "D1",
+    image: require("../assets/mal.jpg"),
+  },
+  {
+    id: "2",
+    title: "T2",
+    description: "D2",
+    image: require("../assets/mal.jpg"),
+  },
+  {
+    id: "3",
+    title: "T3",
+    description: "D3",
+    image: require("../assets/mal.jpg"),
+  },
+];
+
 function MessagesScreen(props) {
   return <FlatList data={[]} />;
 }
