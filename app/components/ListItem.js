4ff128e007141ae7784dcc62f47102c93610844a
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-26 14:05:46 +0500

    adding title styles

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -18,6 +18,7 @@ function ListItem({ title, subTitle, image }) {
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
+    marginVertical: 20,
   },
   image: {
     width: 70,
