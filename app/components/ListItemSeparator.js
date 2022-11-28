612cbf2b65407b326a0fd8405f28d7b0cb6c2e4a
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-28 11:39:58 +0500

    Adding styles to our reusable separator

--- app/components/ListItemSeparator.js
+++ app/components/ListItemSeparator.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { View } from "react-native";
+import { colors } from "../config/colors";
 
 function ListItemSeparator(props) {
   return <View style={{ width: "100%", height: 1, backgroundColor: "#000" }} />;
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  separator: {
+    width: "100%",
+    height: 1,
+    backgroundColor: colors.mediumGray,
+  },
+});
 
 export default ListItemSeparator;
