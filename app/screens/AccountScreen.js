94fbd533e90112290b78c8b9186ed512a140dc78
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-28 14:31:44 +0500

    Adding our components

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -1,19 +1,28 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import ListItem from "../components/ListItem";
 import Screen from "../components/Screen";
 function AccountScreen(props) {
   return (
     <Screen>
-      <ListItem
-        title="John Doe"
-        subTitle="johndoe@gmail.com"
-        image={require("../assets/mal.jpg")}
-      />
+      <View style={styles.container}>
+        <ListItem
+          title="John Doe"
+          subTitle="johndoe@gmail.com"
+          image={require("../assets/mal.jpg")}
+        />
+      </View>
+      <View style={styles.container}>
+        <FlatList />
+      </View>
     </Screen>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 10,
+  },
+});
 
 export default AccountScreen;
