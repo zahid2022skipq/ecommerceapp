a49e0a9c56beb4c15094e1887fde25da2807c231
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-28 13:45:30 +0500

    Implementing Icon Component

--- App.js
+++ App.js
@@ -1,9 +1,4 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import Card from "./app/components/Card";
-import ListingComponentScreen from "./app/screens/ListingComponentScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 
 // <View style={{ backgroundColor: "#f8f4f4", padding: 20, paddingTop: 100 }}>
