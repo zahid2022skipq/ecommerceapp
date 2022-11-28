789b3df67d68ef060fdd67765b65e7c06d0d02c5
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-28 12:10:58 +0500

    Added List item delete action component

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  FlatList,
-  StyleSheet,
-  StatusBar,
-  Platform,
-  SafeAreaView,
-  View,
-} from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 import ListItem from "../components/ListItem";
 import ListItemDeleteAction from "../components/ListItemDeleteAction";
 import ListItemSeparator from "../components/ListItemSeparator";
