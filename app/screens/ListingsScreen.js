660e4058283af396cd08e9c318584871e663b184
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-29 12:41:12 +0500

    Added Listings Screen

---
+++ app/screens/ListingsScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { FlatList, StyleSheet } from "react-native";
+import Screen from "../components/Screen";
+function ListingsScreen(props) {
+  return (
+    <Screen>
+      <FlatList />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ListingsScreen;
