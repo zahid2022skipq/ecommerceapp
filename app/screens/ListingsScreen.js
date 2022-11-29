178f6fecf2644eb87a52ab5f737a7427724ecd0c
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-29 13:03:41 +0500

    adding listings data

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -8,11 +8,13 @@ const listings = [
     id: 1,
     title: "Red Jacket",
     price: "100$",
+    image: require("../assets/jacket.jpg"),
   },
   {
     id: 2,
     title: "Couch in good condition",
     price: "1000$",
+    image: require("../assets/couch.jpg"),
   },
 ];
 
