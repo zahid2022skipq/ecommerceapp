a7b92e2343246220c2b7ce2d2a7b92dc518f86ee
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-29 12:45:01 +0500

    added couch image file

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { FlatList, StyleSheet } from "react-native";
+
 import Screen from "../components/Screen";
+
 function ListingsScreen(props) {
   return (
     <Screen>
