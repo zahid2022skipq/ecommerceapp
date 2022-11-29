e1aca6d5fe6b26efc03261e8f994dd48a2d19766
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-29 14:55:36 +0500

    Custom App Picker Component

---
+++ app/components/AppPicker.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { View, StyleSheet, TextInput, Platform } from "react-native";
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { colors } from "../config/colors";
+
+function AppPicker({ icon, ...otherprops }) {
+  return (
+    <View style={styles.container}>
+      {icon && (
+        <MaterialCommunityIcons
+          name={icon}
+          size={20}
+          color={colors.mediumGray}
+          style={styles.icon}
+        />
+      )}
+      <TextInput {...otherprops} />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.light,
+    borderRadius: 25,
+    flexDirection: "row",
+    padding: 15,
+    width: "100%",
+    marginVertical: 10,
+  },
+  icon: {
+    marginRight: 20,
+  },
+  inputText: {
+    color: colors.dark,
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+  },
+});
+export default AppPicker;
