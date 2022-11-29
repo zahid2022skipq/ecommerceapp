88078fdedd0eaef61ad5ed9d392fe087bb0fee9c
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-29 13:02:46 +0500

    adding listings data

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -3,6 +3,19 @@ import { FlatList, StyleSheet } from "react-native";
 
 import Screen from "../components/Screen";
 
+const listings = [
+  {
+    id: 1,
+    title: "Red Jacket",
+    price: "100$",
+  },
+  {
+    id: 2,
+    title: "Couch in good condition",
+    price: "1000$",
+  },
+];
+
 function ListingsScreen(props) {
   return (
     <Screen>
