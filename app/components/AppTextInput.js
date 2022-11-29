9b972c5cebebc51d19f9734896fd87fad1702dc1
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-29 13:52:27 +0500

    Added Custome App Text Input component file

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -2,14 +2,24 @@ import React from "react";
 import { View, StyleSheet, TextInput } from "react-native";
 
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { colors } from "../config/colors";
 
 function AppTextInput(props) {
   return (
-    <View>
+    <View style={styles.container}>
       <MaterialCommunityIcons />
       <TextInput />
     </View>
   );
 }
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.light,
+    borderRadius: 25,
+    flexDirection: "row",
+    padding: 15,
+    width: "100%",
+    marginVertical: 10,
+  },
+});
 export default AppTextInput;
