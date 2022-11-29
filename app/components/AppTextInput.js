d9883ef599527f83b16064cb0441079193d862f7
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-29 13:49:24 +0500

    Added Custome App Text Input component file

---
+++ app/components/AppTextInput.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, StyleSheet, TextInput } from "react-native";
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+function AppTextInput(props) {
+  return (
+    <View>
+      <MaterialCommunityIcons />
+      <TextInput />
+    </View>
+  );
+}
+const styles = StyleSheet.create({});
+export default AppTextInput;
