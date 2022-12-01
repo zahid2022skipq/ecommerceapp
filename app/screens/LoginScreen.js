ac1ba508e93fbc5257e2d9f0a32fac13e95531be
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-01 12:30:24 +0500

    adding validations

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -7,6 +7,11 @@ import AppButton from "../components/AppButton";
 import { Formik } from "formik";
 import * as Yup from "yup";
 
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
+
 function LoginScreen(props) {
   return (
     <Screen style={styles.conatianer}>
