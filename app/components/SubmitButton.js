eeae2126ec6494706ae84cd7091b014223cad88a
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-01 18:20:51 +0500

    Added submit button file

--- app/components/SubmitButton.js
+++ app/components/SubmitButton.js
@@ -1,9 +1,11 @@
+import { useFormikContext } from "formik";
 import React from "react";
 
 import AppButton from "./AppButton";
 
-function SubmitButton(props) {
-  return <AppButton title="Login" handleBtn={handleSubmit} />;
+function SubmitButton({ title }) {
+  const { handleSubmit } = useFormikContext();
+  return <AppButton title={title} handleBtn={handleSubmit} />;
 }
 
 export default SubmitButton;
