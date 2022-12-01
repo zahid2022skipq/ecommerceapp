7b72327fbd168b611d3705dc24328082461448d7
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-01 17:34:00 +0500

    adding name prop  on blur and change text

--- app/components/AppFormField.js
+++ app/components/AppFormField.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-function AppFormField(props) {
-  return <div></div>;
+function AppFormField({ name }) {
+  return (
+    <>
+      <AppTextInput
+        // autoCorrect={false}
+        // autoCapitalize="none"
+        // icon="email"
+        // keyboardType="email-address"
+        onBlur={() => setFieldTouched(name)}
+        onChangeText={handleChange(name)}
+        // placeholder="Email"
+      />
+      <ErrorMessage error={errors.email} visible={touched.email} />
+    </>
+  );
 }
 
 export default AppFormField;
