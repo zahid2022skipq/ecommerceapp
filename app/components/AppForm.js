a97e9d91e30f5c45a589a557e7ad91d7be1ac7e5
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-02 12:31:30 +0500

    added children to AppForm

--- app/components/AppForm.js
+++ app/components/AppForm.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Formik } from "formik";
 
-function AppForm({ initialValues, onSubmit, validationSchema }) {
+function AppForm({ initialValues, onSubmit, validationSchema, children }) {
   return (
     <Formik
       initialValues={initialValues}
       onSubmit={onSubmit}
       validationSchema={validationSchema}
-    ></Formik>
+    >
+      {() => {
+        <>{children}</>;
+      }}
+    </Formik>
   );
 }
 
