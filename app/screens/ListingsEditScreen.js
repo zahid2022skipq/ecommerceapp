b717c4393feac1fa1e1c49dd7404b94385bbabeb
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-02 12:56:26 +0500

    added screen

--- app/screens/ListingsEditScreen.js
+++ app/screens/ListingsEditScreen.js
@@ -3,6 +3,7 @@ import { StyleSheet } from "react-native";
 import * as Yup from "yup";
 
 import { AppForm, AppFormField, SubmitButton } from "../components/form";
+import Screen from "../components/Screen";
 
 function ListingsEditScreen(props) {
   return <div></div>;
