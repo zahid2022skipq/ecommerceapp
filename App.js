9d2692c18ce4a0074dc4bee74af56a72a9040cdd
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-02 14:08:28 +0500

    improving message screen

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { StyleSheet, Switch, Text, TextInput, View } from "react-native";
 import Screen from "./app/components/Screen";
 import MessagesScreen from "./app/screens/MessagesScreen";
