164042f82fce9f7d665f326d6188725abbbef86c
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-02 12:34:28 +0500

    Refactoring code to maximize optimization

--- app/components/AppForm.js
+++ app/components/AppForm.js
@@ -8,9 +8,7 @@ function AppForm({ initialValues, onSubmit, validationSchema, children }) {
       onSubmit={onSubmit}
       validationSchema={validationSchema}
     >
-      {() => {
-        <>{children}</>;
-      }}
+      {() => <>{children}</>}
     </Formik>
   );
 }
