652b4078e417f130b3c508e990483aaa2dfc997b
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-02 13:59:04 +0500

    handling long texts

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -3,7 +3,7 @@ import { Text, StyleSheet, Platform } from "react-native";
 
 function AppText({ children, style, ...otherProps }) {
   return (
-    <Text style={[styles.text, style]} otherProps>
+    <Text style={[styles.text, style]} {...otherProps}>
       {children}
     </Text>
   );
