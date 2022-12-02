5083f4794b269be32f8a73c9e5d4440707566cd5
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-02 14:08:17 +0500

    improving message screen

--- App.js
+++ App.js
@@ -1,22 +1,7 @@
 import { useState } from "react";
 import { StyleSheet, Switch, Text, TextInput, View } from "react-native";
-import AppPicker from "./app/components/AppPicker";
-import AppTextInput from "./app/components/AppTextInput";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
 import Screen from "./app/components/Screen";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import LoginScreen from "./app/screens/LoginScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
-import ListingsEditScreen from "./app/screens/ListingsEditScreen";
-// <View style={{ backgroundColor: "#f8f4f4", padding: 20, paddingTop: 100 }}>
-// <Card
-//   title="Red Jacket for sell"
-//   subTitle="$100"
-//   image={require("./app/assets/jacket.jpg")}
-// />
-// </View>
 
 export default function App() {
   return (
