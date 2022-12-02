6263dbec5921a62ca6506b01bbfabc834709be6b
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-02 12:29:09 +0500

    added formik in appform

--- app/components/AppForm.js
+++ app/components/AppForm.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Formik } from "formik";
 
 function AppForm(props) {
-  return <div></div>;
+  return (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      onSubmit={(values) => console.log(values)}
+      validationSchema={validationSchema}
+    ></Formik>
+  );
 }
 
 export default AppForm;
