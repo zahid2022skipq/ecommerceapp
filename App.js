21426da0270a644427492c429034586a7097d558
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-03 11:32:26 +0500

    making beautiful picker item

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StyleSheet, Switch, Text, TextInput, View } from "react-native";
+import AppPicker from "./app/components/AppPicker";
 import Screen from "./app/components/Screen";
 import ListingEditScreen from "./app/screens/ListingsEditScreen";
+
+const categories = [
+  {
+    label: "Furniture",
+    value: 1,
+  },
+  {
+    label: "Clothing",
+    value: 1,
+  },
+  {
+    label: "Cameras",
+    value: 1,
+  },
+];
 
 export default function App() {
   return (
     <Screen>
-      <ListingEditScreen />
+      <AppPicker />
     </Screen>
   );
 }
